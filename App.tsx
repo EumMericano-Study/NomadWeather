53ce4a5763b13914d12afaf5f382d068e5da1c94
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-09 18:57:54 +0900

    feat: flex 레이아웃
    View는 기본적으로 display: flex, fled-direction: column
    css가 적용 되어 있는 형태
    
    overflow는 기본적으로 보이지 않는다

--- App.tsx
+++ App.tsx
@@ -1,27 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button, Vibration } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Hello! I made a RN App!</Text>
-            <Button
-                title="3-3-7 진동"
-                onPress={() => Vibration.vibrate([3, 3, 7], true)}
-            />
-            <StatusBar style="light" />
+        <View>
+            <View
+                style={{ width: 200, height: 200, backgroundColor: "tomato" }}
+            ></View>
+            <View
+                style={{ width: 200, height: 200, backgroundColor: "teal" }}
+            ></View>
+            <View
+                style={{ width: 200, height: 200, backgroundColor: "orange" }}
+            ></View>
+            <View
+                style={{ width: 200, height: 200, backgroundColor: "brown" }}
+            ></View>
         </View>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "lightgreen",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    text: {
-        fontSize: 28,
-    },
-});
