c2fb15f57623a8177b8246cb5be3ff2de07e5d49
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-09 19:25:14 +0900

    feat: flex 레아이웃 설정

--- App.tsx
+++ App.tsx
@@ -1,12 +1,50 @@
-import { StyleSheet, View } from "react-native";
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
 export default function App() {
     return (
-        <View style={{ flex: 1 }}>
-            <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
-            <View style={{ flex: 7.5, backgroundColor: "teal" }}></View>
-            <View style={{ flex: 3, backgroundColor: "orange" }}></View>
-            <View style={{ flex: 1, backgroundColor: "brown" }}></View>
+        <View style={styles.container}>
+            <View style={styles.city}>
+                <Text style={styles.cityName}>Seoul</Text>
+            </View>
+            <View style={styles.weather}>
+                <View style={styles.day}>
+                    <Text style={styles.temp}>27</Text>
+                    <Text style={styles.desc}>Sunny</Text>
+                </View>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#69abce",
+    },
+    city: {
+        flex: 1.2,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    cityName: {
+        fontSize: 68,
+        fontWeight: "600",
+    },
+    weather: {
+        flex: 3,
+    },
+    day: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    temp: {
+        fontSize: 178,
+        fontWeight: "600",
+    },
+    desc: {
+        fontSize: 60,
+        marginTop: 50,
+    },
+});
