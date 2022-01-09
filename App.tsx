e8769219b2bf547e90d671291ec0a3f6709a8d48
Eumericano <51465561+EumYoungHyun@users.noreply.github.com>
2022-01-09 19:04:15 +0900

    Learn: flex 기초 설정

--- App.tsx
+++ App.tsx
@@ -2,19 +2,11 @@ import { StyleSheet, View } from "react-native";
 
 export default function App() {
     return (
-        <View>
-            <View
-                style={{ width: 200, height: 200, backgroundColor: "tomato" }}
-            ></View>
-            <View
-                style={{ width: 200, height: 200, backgroundColor: "teal" }}
-            ></View>
-            <View
-                style={{ width: 200, height: 200, backgroundColor: "orange" }}
-            ></View>
-            <View
-                style={{ width: 200, height: 200, backgroundColor: "brown" }}
-            ></View>
+        <View style={{ flex: 1 }}>
+            <View style={{ flex: 1, backgroundColor: "tomato" }}></View>
+            <View style={{ flex: 7.5, backgroundColor: "teal" }}></View>
+            <View style={{ flex: 3, backgroundColor: "orange" }}></View>
+            <View style={{ flex: 1, backgroundColor: "brown" }}></View>
         </View>
     );
 }
